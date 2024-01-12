66aadea980c3bcfa3b5b423f2f04c2688e114110
dvillcar <diego.villa@devzeros.com>
2024-01-12 10:22:26 -0500

    nuevas migraciones

---
+++ Content/Infrastructure/Migrations/20240112152143_initial-content.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class initialcontent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Content");
+
+            migrationBuilder.EnsureSchema(
+                name: "Base");
+
+            migrationBuilder.CreateTable(
+                name: "Content",
+                schema: "Content",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Tag = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    LogoUrl = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Multimedia = table.Column<string>(type: "nvarchar(max)", maxLength: 5000, nullable: true),
+                    Languages = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    Items = table.Column<string>(type: "nvarchar(max)", maxLength: 8000, nullable: true),
+                    DeletedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    LastModifiedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Content", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContentIds",
+                schema: "Base",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContentIds", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Content_Tag",
+                schema: "Content",
+                table: "Content",
+                column: "Tag",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Content",
+                schema: "Content");
+
+            migrationBuilder.DropTable(
+                name: "ContentIds",
+                schema: "Base");
+        }
+    }
+}
